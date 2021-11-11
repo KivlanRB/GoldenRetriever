268c32138bcfc2678233fecc6ed1576b9ebe45d9
KivlanRB <kivlantrc@ragtag.moe>
2021-11-11 17:23:25 +0700

    add keyring feature

---
+++ API/storesafe.py
@@ -0,0 +1,31 @@
+import keyring
+import codecs
+from getpass import getpass
+
+from keyring.core import get_password
+
+class StoreSafe(object):
+    """Provides methods to securely store and retrieve passwords from keyring"""
+
+    def __init__(self, service_id:str) -> None:
+        super().__init__()
+        self.service_id = service_id
+
+    def _set_creds(self, un:str, pw:str):
+        hashed = codecs.encode(pw, "rot_13")
+        keyring.set_password(self.service_id, "username", un)
+        keyring.set_password(self.service_id, "password", hashed)
+    
+    def _get_creds(self):
+        uname = keyring.get_password(self.service_id, "username")
+        pword = keyring.get_password(self.service_id, "password")
+        if (not uname) or (not pword):
+            return None, None
+        pword = codecs.decode(pword, "rot_13")
+        return uname, pword
+
+if __name__ == "__main__":
+    uname = input("username: ")
+    pword = input("password: ")
+    uac = StoreSafe("Cool.Who")
+    print(uac._get_creds())
